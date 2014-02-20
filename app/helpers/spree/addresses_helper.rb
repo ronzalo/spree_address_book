a02703134fa0a1427e655a3ba62290e90f9ded99
Gonzalo <gmoreno@acid.cl>
2014-02-20 19:24:41 -0300

    update helpers to bootstrap

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -7,7 +7,7 @@ module Spree::AddressesHelper
         is_required = Spree::Address.required_fields.include?(method)
         separator = is_required ? '<span class="required">*</span><br />' : '<br />'
         form.label(method) + separator.html_safe +
-        form.text_field(method, :class => is_required ? 'required' : nil)
+        form.text_field(method, :class => is_required ? 'form-control required' : nil)
       end
     end
   end
@@ -19,16 +19,16 @@ module Spree::AddressesHelper
       form.collection_select(:state_id, country.states.order(:name),
                             :id, :name,
                             {:include_blank => true},
-                            {:class => have_states ? "required" : "hidden",
+                            {:class => have_states ? "form-control required" : "hidden",
                             :disabled => !have_states}) +
       form.text_field(:state_name,
-                      :class => !have_states ? "required" : "hidden",
+                      :class => !have_states ? "form-control required" : "hidden",
                       :disabled => have_states)
       ].join.gsub('"', "'").gsub("\n", "")
 
     form.label(:state, Spree.t(:state)) + 
       %Q(<span class="required" id="#{address_id}state-required">*</span><br />).html_safe +
-      content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
+      content_tag(:noscript, form.text_field(:state_name, :class => 'form-control required')) +
       javascript_tag("document.write(\"#{state_elements.html_safe}\");")
   end
 end
