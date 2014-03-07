b2339cbe16f2a72bbbce2f1ab033ba373cdb8a6d
Gonzalo <gmoreno@acid.cl>
2014-03-07 16:12:51 -0300

    fix bugs

--- app/assets/javascripts/store/spree_address_book.js
+++ app/assets/javascripts/store/spree_address_book.js
@@ -1,56 +1,71 @@
 (function($) {
+
   $(document).ready(function(){
-    if ($(".select_address").length) {
+
+    $("#order_bill_address_id").change(function(){
+      if ($.isNumeric($(this).val())) {
+        hide_address_form("billing");
+        $("#new_billing_address").click(function(){
+          show_address_form('billing');
+          return false;
+        });
+      } else {
+        show_address_form("billing");
+      }
+    });
+
+    if ($.isNumeric($("#order_bill_address_id").val())) {
       $('input#order_use_billing').unbind("click");
-      $("#shipping .inner").hide();
+      $(".inner").hide();
       $(".inner input").prop("disabled", true);
       $(".inner select").prop("disabled", true);
-      if ($('input#order_use_billing').is(':checked')) {
+      if ($('input#order_use_billing').is(':checked'))
         $("#shipping .select_address").hide();
-      }
       
       $('input#order_use_billing').click(function() {
-        if ($(this).is(':checked')) {
+        if ($(this).is(':checked'))
           $("#shipping .select_address").hide();
-          _hide_address_form('shipping');
-        } else {
+        else {
           $("#shipping .select_address").show();
-          if ($("#new_shipping_address").data('active') == 0)
-            _show_address_form('shipping');
-          else
-            _hide_address_form('shipping');
+          _hide_address_form('shipping');
+          $("#new_shipping_address").show();
         }
       });
 
       $("#new_billing_address").click(function(){
-        show_address_form(this, 'billing');
+        show_address_form('billing');
         return false;
       });
 
       $("#new_shipping_address").click(function(){
-        show_address_form(this, 'shipping');
+        show_address_form('shipping');
         return false;
       });
+    }
 
-      $("#order_ship_address_id").change(function(){
-        hide_address_form('shipping');
-      });
-
-      $("#order_bill_address_id").change(function(){
-        hide_address_form('billing');
-      });
+    function hide_address_form(address_type){
+       $("#new_" + address_type + "_address").show();
+      _hide_address_form(address_type);
+    }
+    
+    function show_address_form(address_type){
+      $("#new_" + address_type + "_address").hide()
+      $("#order_" + address_type.substring(0, 4) + "_address_id").val(null);
+      _show_address_form(address_type);
+    }
+    
+    function _hide_address_form(address_type){
+      $("#" + address_type + " .inner").hide();
+      $("#" + address_type + " .inner input").prop("disabled", true);
+      $("#" + address_type + " .inner select").prop("disabled", true);
+    }
+    
+    function _show_address_form(address_type){
+      $("#" + address_type + " .inner").show();
+      $("#" + address_type + " .inner input").prop("disabled", false);
+      $("#" + address_type + " .inner select").prop("disabled", false);
     }
   });
+
   
-  function _hide_address_form(address_type){
-    $("#" + address_type + " .inner").hide();
-    $("#" + address_type + " .inner input").prop("disabled", true);
-    $("#" + address_type + " .inner select").prop("disabled", true);
-  }
-  
-  function _show_address_form(address_type){
-    $("#" + address_type + " .inner").show();
-    $("#" + address_type + " .inner input").prop("disabled", false);
-    $("#" + address_type + " .inner select").prop("disabled", false);
-  }
 })(jQuery);
